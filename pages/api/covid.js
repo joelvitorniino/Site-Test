f07ba17fd5ffcefc00173f2fbb5f3d9354c38f3f
joelvitorniino <joelprofessor13@gmail.com>
2021-04-07 23:16:05 -0300

    bah

--- pages/api/covid.js
+++ pages/api/covid.js
@@ -4,6 +4,15 @@ async function Covid(request, response) {
     const cases = covidJSON.cases;
     const deaths = covidJSON.deaths;
     const dynamicDate = new Date();
+    const headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+  headers.append('Accept', 'application/json');
+
+  headers.append('Access-Control-Allow-Origin', 'http://192.168.42.208:5500/index.html');
+  headers.append('Access-Control-Allow-Credentials', 'true');
+
+  headers.append('GET', 'POST', 'OPTIONS');
 
     response.json({
         date: dynamicDate.toGMTString(),
